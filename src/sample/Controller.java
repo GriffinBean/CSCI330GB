7c688a4bcf643fb2499b8a59cc251dc35a7a856e
Griffin Bean <gtbean5419@ung.edu>
2018-08-30 12:10:24 -0400

    Added Model, GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextField email, username, password;
+
+    public void TouchLogin(javafx.event.ActionEvent actionEvent)
+    {
+        Model model = new Model();
+        String hashPassword = model.generatePasswordHash(password.getText());
+        System.out.println(hashPassword);
+    }
 }
